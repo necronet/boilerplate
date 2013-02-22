0a9b41272e83276275edd71834414f41052bb570
Juan Carlos Picado <juan@encuestame.org>
2013-02-22 23:27:12 +0100

    core file

---
+++ core.js
@@ -0,0 +1,17 @@
+(function(w){
+
+	var NAME = {
+		get : function () {
+			return "get";
+		},
+		del : function() {
+			return "del";
+		},
+		put : function () {
+			return "put";
+		}
+	};
+
+	return w.N = NAME;
+
+})(window);
